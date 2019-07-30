59f5a3f6847d7ff265c41d4444e076ff424b78b3
vinaykaikala <vinay.kaikala.ki@gmail.com>
2019-07-30 17:06:12 +0200

    AWS: aws_utils module added to test the s3 access with docker

---
+++ autoseq/aws_utils/get_files.py
@@ -0,0 +1,94 @@
+import json
+import logging
+import os
+import sys
+import time
+import subprocess
+import shlex
+import pathlib
+
+class Awscli():
+    """Get required files from s3 to run the pipeline"""
+    def __init__(self, sampledata, refdata, outdir, libdir, s3bucket='probio-genome'):
+
+        """
+        :param sampledata:
+        :param refdata:
+        :param outdir:
+        :param libdir:
+        :param s3bucket:
+
+        the base directory path should be
+        base: /nfs/PROBIO
+        Fastq : /nfs/PROBIO/INBOX/
+        outputs: /nfs/PROBIO/autoseq-output/<sdid>
+        confs: /nfs/PROBIO/config/<sdid>.json
+        """
+
+        self.base_dir = '/nfs/PROBIO'
+        self.s3bucket = s3bucket
+        self.outdir = outdir
+        self.sampledata = json.load(sampledata)
+        self.sampledata_dir = os.path.dirname(self.sampledata)
+        self.refdata = refdata
+        self.refdata_dir = os.path.dirname(refdata)
+        self.libdir = libdir
+
+        #check and create directories for autoseq pipeline
+        self.check_and_create_dir(self, self.base_dir)
+        self.check_and_create_dir(self, self.outdir)
+        self.check_and_create_dir(self, self.libdir)
+        self.check_and_create_dir(self, self.refdata_dir)
+        self.check_and_create_dir(self, self.sampledata_dir)
+        # check if output directory exist  if not create it
+
+
+
+    def get_common(self):
+        """Get common files required for all steps"""
+        cmd = "aws s3 ls s3://" + self.s3bucket
+        self.run_awscmd(cmd)
+
+    def get(self, step):
+        """Get the files from s3 for given step"""
+        pass
+
+    def put(self):
+        pass
+
+    def download_folder(s3_path, directory_to_download):
+        """
+        Downloads a folder from s3
+        :param s3_path: s3 folder path
+        :param directory_to_download: path to download the directory to
+        :return: directory that was downloaded
+        """
+        cmd = 'aws s3 cp --recursive %s %s' % (s3_path, directory_to_download)
+
+    def run_awscmd(self, cmd):
+        #add conda env
+        cmd = 'source activate awscli && ' + cmd
+        subprocess.check_call(shlex.split(cmd))
+        return True
+
+    def get_all_clinseq_barcodes(self):
+        """
+        :return: All clinseq barcodes included in this clinseq analysis pipeline's panel data.
+        """
+        all_clinseq_barcodes = \
+            self.sampledata['T'] + \
+            self.sampledata['N'] + \
+            self.sampledata['CFDNA']
+        return filter(lambda bc: bc != None, all_clinseq_barcodes)
+
+    def check_and_create_dir(self, dirname):
+        """check if base path is same for current step"""
+        if not os.path.join(*pathlib.Path(dirname).parts[0:3]) == self.base_dir:
+            raise Exception('base path should be /nfs/PROBIO')
+
+        try:
+            os.makedirs(dirname)
+        except Exception as e:
+            return dirname
+        return dirname
+
--- autoseq/cli/liqbio.py
+++ autoseq/cli/liqbio.py
@@ -9,6 +9,7 @@ import click
 from autoseq.pipeline.liqbio import LiqBioPipeline
 from autoseq.util.clinseq_barcode import extract_clinseq_barcodes, convert_barcodes_to_sampledict, validate_clinseq_barcodes
 from autoseq.util.path import mkdir
+from autoseq.aws_utils.get_files import Awscli
 
 
 @click.command()
@@ -25,31 +26,35 @@ def liqbio(ctx, step, sample ):
 
     if ctx.obj['jobdb']:
         mkdir(os.path.dirname(ctx.obj['jobdb']))
-    
-    ctx.obj['pipeline'] = LiqBioPipeline(sampledata=sampledata,
-                                        refdata=ctx.obj['refdata'],
-                                        job_params=ctx.obj['job_params'],
-                                        outdir=ctx.obj['outdir'],
-                                         libdir=ctx.obj['libdir'],
-                                         maxcores=ctx.obj['cores'],
-                                         runner=ctx.obj['runner'],
-                                         jobdb=ctx.obj['jobdb'],
-                                         dot_file=ctx.obj['dot_file'],
-                                         umi=ctx.obj['umi'],
-                                         scratch=ctx.obj['scratch'],
-                                         )
 
-    step_status = ctx.obj['pipeline'].runaws(step)
-    if not step_status:
-        logging.info("No Step configured with step name: " + step + ' check step_to_run dict to fix error')
-        sys.exit(400)
+    #get required files s3 before we start the pipeline
+    aws_cli =  Awscli(sample, ctx.obj['refdata'], ctx.obj['outdir'], ctx.obj['libdir'], s3bucket='probio-genome')
+    aws_cli.get_common()
+    print("Listed all the aws files")
+    #ctx.obj['pipeline'] = LiqBioPipeline(sampledata=sampledata,
+    #                                    refdata=ctx.obj['refdata'],
+    #                                    job_params=ctx.obj['job_params'],
+    #                                    outdir=ctx.obj['outdir'],
+    #                                     libdir=ctx.obj['libdir'],
+    #                                     maxcores=ctx.obj['cores'],
+    #                                     runner=ctx.obj['runner'],
+    #                                     jobdb=ctx.obj['jobdb'],
+    #                                     dot_file=ctx.obj['dot_file'],
+    #                                     umi=ctx.obj['umi'],
+    #                                     scratch=ctx.obj['scratch'],
+    #                                     )
+
+    #step_status = ctx.obj['pipeline'].runaws(step)
+    #if not step_status:
+    #    logging.info("No Step configured with step name: " + step + ' check step_to_run dict to fix error')
+    #    sys.exit(400)
 
     # start main analysis
-    ctx.obj['pipeline'].start()
-    logging.info("Waiting for pipeline to finish.")
-    while ctx.obj['pipeline'].is_alive():
-        logging.debug("Waiting for LiqBioPipeline")
-        time.sleep(5)
+    #ctx.obj['pipeline'].start()
+    #logging.info("Waiting for pipeline to finish.")
+    #while ctx.obj['pipeline'].is_alive():
+    #    logging.debug("Waiting for LiqBioPipeline")
+    #    time.sleep(5)
 
     # return_code from run_pipeline() will be != 0 if the pipeline fails
     #sys.exit(ctx.obj['pipeline'].exitcode)
